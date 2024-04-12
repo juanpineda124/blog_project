c76d527575b83f4bae727e4698301526838675e9
juanpineda124 <juangabriel963@gmail.com>
2024-04-11 21:30:56 -0500

    :sparkles:

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-9">
             <div class="card">
                 <div class="card-header">{{ __('Panel') }}</div>
 
@@ -20,49 +20,62 @@
                 
                     <div class="form-group">
                         <h3 class="text-center">Ingresa a la vista que deseas</h3>
-                        <div class="card mx-2 my-5" style="width: 18rem;">
-                            <img src="{{ asset('images/crear.jpg') }}" class="card-img-top" alt="imagen vista crear">
-                            <div class="card-header">
-                                Crear categoria
-                            </div>
-                            <div class="card-body">
-                                <p class="card-text">Ingresa para crear y guardar la categoria deseada.</p>
-                                <a href="{{ route('categories.create') }}" class="btn btn-primary">Ingresar</a>
-                            </div>
-                        </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="card mb-2 mx-1">
+                                        <img src="{{ asset('images/crear.jpg') }}" class="card-img-top" alt="imagen vista crear">
+                                        <div class="card-header">
+                                            Crear categoria
+                                        </div>
+                                        <div class="card-body">
+                                        <p class="card-text">Ingresa para crear y guardar la categoria deseada.</p>
+                                            <a href="{{ route('categories.create') }}" class="btn btn-primary">Ingresar</a>
+                                        </div>
+                                    </div>
+                                </div>
 
-                        <div class="card mx-2 my-5" style="width: 18rem;">
-                            <img src="{{ asset('images/index.jpg') }}" class="card-img-top" alt="imagen vista index">
-                            <div class="card-header">
-                                Lista de categorias
+                                <div class="col-md-6">
+                                    <div class="card mb-2 mx-1">
+                                        <img src="{{ asset('images/index.jpg') }}" class="card-img-top" alt="imagen vista index">
+                                        <div class="card-header">
+                                            Lista de categorias
+                                        </div>
+                                        <div class="card-body">
+                                            <p class="card-text">Ingresa y visualiza las categorias creadas.</p>
+                                            <a href="{{ route('categories.index') }}" class="btn btn-primary">Ingresar</a>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="card-body">
-                                <p class="card-text">Ingresa y visualiza las categorias creadas.</p>
-                                <a href="{{ route('categories.index') }}" class="btn btn-primary">Ingresar</a>
-                            </div>
-                        </div>
 
-                        <div class="card mx-2 my-5" style="width: 18rem;">
-                            <img src="{{ asset('images/pcrear.png') }}" class="card-img-top" alt="imagen vista post crear">
-                            <div class="card-header">
-                                Crear publicacion
+                            <div class="row">
+                                <div class="col-md-6">
+                                     <div class="card mb-2 mx-2">
+                                        <img src="{{ asset('images/pcrear.png') }}" class="card-img-top" alt="imagen vista post crear">
+                                        <div class="card-header">
+                                            Crear publicacion
+                                        </div>
+                                        <div class="card-body">
+                                            <p class="card-text">Ingresa y crea una plublicación a la categoria deseada.</p>
+                                            <a href="{{ route('posts.create') }}" class="btn btn-primary">Ingresar</a>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="card-body">
-                                <p class="card-text">Ingresa y crea una plublicación a la categoria deseada.</p>
-                                <a href="{{ route('posts.create') }}" class="btn btn-primary">Ingresar</a>
-                            </div>
-                        </div>
 
-                        <div class="card mx-2 my-5" style="width: 18rem;">
-                            <img src="{{ asset('images/pindex.png') }}" class="card-img-top" alt="imagen vista post crear">
-                            <div class="card-header">
-                                Listar publicaciones
+                            <div class="col-md-6">
+                                 <div class="card mb-2 mx-1">
+                                    <img src="{{ asset('images/pindex.png') }}" class="card-img-top" alt="imagen vista post crear">
+                                    <div class="card-header">
+                                        Listar publicaciones
+                                    </div>
+                                    <div class="card-body">
+                                        <p class="card-text">Ingresa y visualiza la lista de publicaciones que tienen las diferentes categorias.</p>
+                                        <a href="{{ route('posts.index') }}" class="btn btn-primary">Ingresar</a>
+                                    </div>
+                                 </div>
                             </div>
-                            <div class="card-body">
-                                <p class="card-text">Ingresa y visualiza la lista de publicaciones que tienen las diferentes categorias.</p>
-                                <a href="{{ route('posts.index') }}" class="btn btn-primary">Ingresar</a>
-                            </div>
-                        </div>
+                         </div>
                     </div>
                 </div>   
             </div>
